d1e541e645a0b57f3f02d01b8cb7d08513ddf25a
BolterPrimarisMe <123117918+BolterPrimarisMe@users.noreply.github.com>
2023-10-04 09:56:10 -0600

    Add files via upload

---
+++ Javaproject.java/src/Horse.java
@@ -0,0 +1,56 @@
+public class Horse {
+        // Instance variables
+        private String name;
+        private int age;
+        private String color;
+    
+        // Constructor
+        public Horse(String name, int age, String color) {
+            this.name = name;
+            this.age = age;
+            this.color = color;
+        }
+    
+        // Getter methods
+        public String getName() {
+            return name;
+        }
+    
+        public int getAge() {
+            return age;
+        }
+    
+        public String getColor() {
+            return color;
+        }
+    
+        // Setter methods
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public void setAge(int age) {
+            this.age = age;
+        }
+    
+        public void setColor(String color) {
+            this.color = color;
+        }
+    
+        // Method to make the horse gallop
+        public void gallop() {
+            System.out.println(name + " is galloping!");
+        }
+    
+        // Method to display horse information
+        public String toString() {
+            String outputt="";
+            outputt=outputt +"Name: " + name;
+            outputt=outputt +"Age: " + age + " years";
+            outputt=outputt +"Color: " + color;
+            return outputt;
+        }
+    }
+
+
+
--- Javaproject.java/src/hrse.java
+++ Javaproject.java/src/hrse.java
@@ -3,62 +3,11 @@
 // Done for Software Devolopment
 // This is for Objects week 6
 public class hrse {
-     public class  Horse {
-            // Instance variables
-            private String name;
-            private int age;
-            private String color;
-        
-            // Constructor
-            public Horse(String name, int age, String color) {
-                this.name = name;
-                this.age = age;
-                this.color = color;
-            }
-        
-            // Getter methods
-            public String getName() {
-                return name;
-            }
-        
-            public int getAge() {
-                return age;
-            }
-        
-            public String getColor() {
-                return color;
-            }
-        
-            // Setter methods
-            public void setName(String name) {
-                this.name = name;
-            }
-        
-            public void setAge(int age) {
-                this.age = age;
-            }
-        
-            public void setColor(String color) {
-                this.color = color;
-            }
-        
-            // Method to make the horse gallop
-            public void gallop() {
-                System.out.println(name + " is galloping!");
-            }
-        
-            // Method to display horse information
-            public void displayInfo() {
-                System.out.println("Name: " + name);
-                System.out.println("Age: " + age + " years");
-                System.out.println("Color: " + color);
-            }
-        }
-    
-    
+  
     public static void main(String[] args){
+        // the Horse 1
     Horse horse1=new Horse("horse1", 10, "red");
-    
+         
     Horse horse2=new Horse("horse2", 10, "red");
     
     Horse horse3=new Horse("horse3", 10, "red");
