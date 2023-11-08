8e2ecc458a3233cf3e79332d194c958dc41cd519
BolterPrimarisMe <123117918+BolterPrimarisMe@users.noreply.github.com>
2023-11-08 10:02:50 -0700

    Add files via upload

--- Javaproject.java/src/meatRabbit.java
+++ Javaproject.java/src/meatRabbit.java
@@ -1,18 +1,22 @@
+// extension of rabbit 1 uses rabbit one as a basis for program
 public class meatRabbit extends rabbit1  {
     private int weight; 
     private String feedtype;
     public meatRabbit (String name, int age, String feedtype, int weight){
+        // creates the different varibles for the meat rabbit
         super(name, age);
         this.feedtype=feedtype;
         this.weight=weight;
-
+// feed type is registerd
 }
       public void feed(int feed){
       weight=weight+feed;
       }
+      // weight is registerd
       public int getWeight(){
       return weight;
       }
+      //name is registerd
       public String toString() {
         return "Rabbit(name='" + name + "', age=" + age + "this rabbit eats "+feedtype;
     }
--- Javaproject.java/src/raceRabbit.java
+++ Javaproject.java/src/raceRabbit.java
@@ -1,17 +1,22 @@
+// extension of rabbit 1 uses rabbit one as a basis for program
 public class raceRabbit extends rabbit1{
     private int speed; 
     private String dexterity;
     public raceRabbit (String name, int age, String dexterity, int speed){
+                // creates the different varibles for the race rabbit
         super(name, age);
         this.dexterity=dexterity;
         this.speed=speed;
 }
+// the exsersie is created
       public void excersise(int excersise){
       speed=speed+excersise;
       }
+      // speed is created
       public int getspeed(){
       return speed;
       }
+      //name is created
       public String toString() {
       return "Rabbit(name='" + name + "', age=" + age + "this rabbit is fast"+dexterity;
       }
